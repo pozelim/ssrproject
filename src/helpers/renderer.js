9f098588b674bb07460c36cbd6eb57e0cc026c13
Pedro Ozelim <pedrohozelim@gmail.com>
2018-04-30 00:29:22 -0300

    creating renderer with react render to string

---
+++ src/helpers/renderer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+export default () => {
+const content = renderToString(<h1>Hello</h1>);
+    return `
+        <html>
+            <header></header>
+            <body>
+                <div id="content">
+                    ${content}
+                </div>
+            </body>
+        </html>
+    `;
+};
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import renderer from './helpers/renderer'
 
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("HI %)");
+    res.send(renderer());
 });
-
+  
 app.listen(3000, () => {
     console.log("listening on port 3000");
 });
