a83262b954da6b6ae50e069dfa0804cab9077e18
Pedro Ozelim <pedrohozelim@gmail.com>
2018-04-30 01:20:34 -0300

    Adding bundle client to be load

--- src/helpers/renderer.js
+++ src/helpers/renderer.js
@@ -17,6 +17,7 @@ const content = renderToString(
                 <div id="root">
                     ${content}
                 </div>
+                <script src='bundle.js'></script>
             </body>
         </html>
     `;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import renderer from './helpers/renderer'
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.send(renderer());
 });
-  
+ 
 app.listen(3000, () => {
     console.log("listening on port 3000");
 });
